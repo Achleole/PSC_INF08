2be47b8939ae60350707467707c5373776c2f831
a-del <adelrocq@laposte.net>
2018-01-28 22:08:02 +0100

    create unit tests for class CPU

---
+++ utests/utest_cpu.py
@@ -0,0 +1,131 @@
+import unittest
+import Univers
+import CPU
+
+class TestCPU(unittest.TestCase):
+    def setUp(self):
+        self.u = Univers.Univers()
+
+    def test_creationCPU(self) :
+        stack = [i for i in range(CPU.TAILLE_STACK)]
+        c = CPU.CPU(102, self.u, 3,7,9,11, stack,2)
+        self.assertEqual(c.ptr, 102)
+        self.assertEqual(c.ax, 3)
+        self.assertEqual(c.bx,7)
+        self.assertEqual(c.cx,9)
+        self.assertEqual(c.dx,11)
+        for i in range(CPU.TAILLE_STACK) :
+            self.assertEqual(c.stack[c.stack_ptr],(i+2)%CPU.TAILLE_STACK)
+            c.incrementer_stack_ptr()
+
+    def test_incr_stack(self) :
+        c = CPU.CPU(102, self.u)
+        p = c.stack_ptr
+        c.incrementer_stack_ptr()
+        self.assertTrue(c.stack_ptr==p+1or(c.stack_ptr==0&p==CPU.TAILLE_STACK-1))
+        c.stack_ptr = 1
+        c.incrementer_stack_ptr()
+        self.assertEqual(c.stack_ptr, 2)
+
+    def test_incr_ptr(self) :
+        c = CPU.CPU(102, self.u)
+        p = c.ptr
+        c.incrementer_ptr()
+        if p==len(self.u.memoire)-1 :
+            self.assertEqual(c.ptr,0)
+        else :
+            self.assertEqual(c.ptr,p+1)
+        c.ptr = len(self.u.memoire)-1
+        p = c.ptr
+        c.incrementer_ptr()
+        self.assertEqual(c.ptr, 0)
+
+    def test_decr_stack(self) :
+        c = CPU.CPU(102, self.u)
+        p = c.stack_ptr
+        c.decrementer_stack_ptr()
+        if p == 0 :
+            self.assertEqual(c.stack_ptr, CPU.TAILLE_STACK - 1)
+        else :
+            self.assertEqual(c.stack_ptr, p - 1)
+        c.stack_ptr = 1
+        c.incrementer_stack_ptr()
+        self.assertEqual(c.stack_ptr, 2)
+
+    def test_die(self) :
+        c1 = CPU.CPU(102, self.u)
+        c2 = CPU.CPU(0, self.u)
+        c3 = CPU.CPU(101, self.u)
+        c4 = CPU.CPU(102, self.u)
+        self.u.inserer_cpu(c1)
+        self.u.inserer_cpu(c2)
+        self.u.inserer_cpu(c3)
+        self.u.inserer_cpu(c4)
+        c1.die()
+        self.assertFalse(c1 in self.u.liste_cpus)
+        self.assertTrue((c1.ptr not in self.u.localisation_cpus) or (c1 not in self.u.localisation_cpus[c1.ptr]))
+        c2.die()
+        self.assertFalse(c2 in self.u.liste_cpus)
+        self.assertTrue((c2.ptr not in self.u.localisation_cpus) or (c2 not in self.u.localisation_cpus[c2.ptr]))
+        c4.die()
+        self.assertFalse(c4 in self.u.liste_cpus)
+        self.assertTrue((c4.ptr not in self.u.localisation_cpus) or (c4 not in self.u.localisation_cpus[c4.ptr]))
+        c3.die()
+        self.assertFalse(c3 in self.u.liste_cpus)
+        self.assertTrue((c3.ptr not in self.u.localisation_cpus) or (c3 not in self.u.localisation_cpus[c3.ptr]))
+
+    def test_push_stack(self) :
+        c = CPU.CPU(102, self.u)
+        c.push_stack(3)
+        self.assertEqual(c.stack_ptr,0)
+        self.assertEqual(c.stack[c.stack_ptr],3)
+        c.push_stack(2)
+        for i in range(1,CPU.TAILLE_STACK) :
+            self.assertEqual(c.stack[i],0)
+        self.assertEqual(c.stack_ptr, 0)
+        self.assertEqual(c.stack[c.stack_ptr], 2)
+        c.incrementer_stack_ptr()
+        c.push_stack(-5)
+        self.assertEqual(c.stack_ptr, 1)
+        self.assertEqual(c.stack[c.stack_ptr], -5)
+
+    def test_pop_stack(self):
+        c = CPU.CPU(102, self.u, stack=[0]*CPU.TAILLE_STACK)
+        self.assertEqual(c.stack_ptr, 0)
+        self.assertEqual(0,c.pop_stack())
+        c.stack = [i for i in range(CPU.TAILLE_STACK)]
+        self.assertEqual(CPU.TAILLE_STACK-1, c.pop_stack()) #On verifie que pop_stack renvoie la bonne valeur...
+        for i in range(CPU.TAILLE_STACK) :# ...et qu'il ne change pas les valeurs
+            self.assertEqual(i, c.stack[i])
+        c.push_stack(3)
+        self.assertEqual(CPU.TAILLE_STACK - 1, c.pop_stack())
+        c.incrementer_stack_ptr()
+        self.assertEqual(c.stack_ptr, 1)
+        self.assertEqual(3, c.pop_stack())
+        self.assertEqual(c.stack_ptr, 1)
+
+    def test_ajouter_localisation(self):
+        c = CPU.CPU(102, self.u)
+        c.ajouter_localisation()
+        self.assertTrue(102 in self.u.localisation_cpus and c in self.u.localisation_cpus[102])
+        c2 = CPU.CPU(102, self.u)
+        c3 = CPU.CPU(102, self.u)
+        c3.ajouter_localisation()
+        c2.ajouter_localisation()
+        #Pour verifier que l'ajout de CPUs n'en retire pas d'autres :
+        self.assertTrue(102 in self.u.localisation_cpus and c in self.u.localisation_cpus[102])
+        self.assertTrue(102 in self.u.localisation_cpus and c2 in self.u.localisation_cpus[102])
+        self.assertTrue(102 in self.u.localisation_cpus and c3 in self.u.localisation_cpus[102])
+
+    def test_enlever_localisation(self):
+        c1 = CPU.CPU(102, self.u)
+        c2 = CPU.CPU(102, self.u)
+        c3 = CPU.CPU(102, self.u)
+        c1.ajouter_localisation()
+        c3.ajouter_localisation()
+        c2.ajouter_localisation()
+        c1.enlever_localisation()
+        self.assertTrue(c1 not in self.u.localisation_cpus[102] and c2 in self.u.localisation_cpus[102] and c3 in self.u.localisation_cpus[102])
+
+
+    # Comment tester execute ??
