0262bd8cca1c798dc5bceb8586c5cd5b6afc97c1
a-del <adelrocq@laposte.net>
2018-02-09 23:16:04 +0100

    changed all indents to spaces

--- Enregistrement.py
+++ Enregistrement.py
@@ -1,136 +1,136 @@
 from math import *
 from CPU import *
 def charger_genome(fichier):
-	genome = []
-	"Renvoie sous forme de tableau de strings le genome specifie dans le fichier"
-	f = open(fichier, 'r')
-	for line in f:
-		genome.append(line.strip())
-	return genome
+    genome = []
+    "Renvoie sous forme de tableau de strings le genome specifie dans le fichier"
+    f = open(fichier, 'r')
+    for line in f:
+        genome.append(line.strip())
+    return genome
 
 def CPUtoInt(cpu):
-	k=ceil(log(CPU.TAILLE_STACK,2))
-	# if cpu.ax>2**(cpu.univers.n3):
-	# 	print("erreur ax")
-	# if cpu.bx>2**(cpu.univers.n3):
-	# 	print("erreur bx")
-	# if cpu.cx>2**(cpu.univers.n3):
-	# 	print("erreur cx")
-	# if cpu.dx>2**(cpu.univers.n3):
-	# 	print("erreur dx")
-	# if cpu.ptr>2**(cpu.univers.n3):
-	# 	print("erreur ptr")
-	resultat=cpu.ax
-	resultat=(resultat<<cpu.univers.n3)+cpu.bx
-	resultat=(resultat<<cpu.univers.n3)+cpu.cx
-	resultat=(resultat<<cpu.univers.n3)+cpu.dx
-	resultat=(resultat<<cpu.univers.n3)+cpu.ptr
-	for i in cpu.stack:
-		resultat=(resultat<<cpu.univers.n2)+i
-	resultat=(resultat<<k)+cpu.stack_ptr
-	return resultat
-	
+    k=ceil(log(CPU.TAILLE_STACK,2))
+    # if cpu.ax>2**(cpu.univers.n3):
+    # 	print("erreur ax")
+    # if cpu.bx>2**(cpu.univers.n3):
+    # 	print("erreur bx")
+    # if cpu.cx>2**(cpu.univers.n3):
+    # 	print("erreur cx")
+    # if cpu.dx>2**(cpu.univers.n3):
+    # 	print("erreur dx")
+    # if cpu.ptr>2**(cpu.univers.n3):
+    # 	print("erreur ptr")
+    resultat=cpu.ax
+    resultat=(resultat<<cpu.univers.n3)+cpu.bx
+    resultat=(resultat<<cpu.univers.n3)+cpu.cx
+    resultat=(resultat<<cpu.univers.n3)+cpu.dx
+    resultat=(resultat<<cpu.univers.n3)+cpu.ptr
+    for i in cpu.stack:
+        resultat=(resultat<<cpu.univers.n2)+i
+    resultat=(resultat<<k)+cpu.stack_ptr
+    return resultat
+
 def intToCPU(entier,univers):
-	k=ceil(log(CPU.TAILLE_STACK,2))
-	bits0=0
-	for i in range(univers.n3):
-		bits0+=2**i
-	bits1=0
-	for i in range(k):
-		bits1+=2**i
-	bits2=0
-	for i in range(univers.n2):
-		bits2+=2**i
-	stack_ptr=entier & bits1		
-	stack=[]
-	for i in range(CPU.TAILLE_STACK):
-		stack.insert(0,(entier>>(k+i*univers.n2))&bits2)
-	ptr=(entier>>(k+(CPU.TAILLE_STACK)*univers.n2))&bits0
-	dx=(entier>>(k+CPU.TAILLE_STACK*univers.n2+1*univers.n3))&bits0
-	cx=(entier>>(k+CPU.TAILLE_STACK*univers.n2+2*univers.n3))&bits0
-	bx=(entier>>(k+CPU.TAILLE_STACK*univers.n2+3*univers.n3))&bits0
-	ax=(entier>>(k+CPU.TAILLE_STACK*univers.n2+4*univers.n3))&bits0
-	return CPU(ptr, univers, ax, bx, cx, dx,stack , stack_ptr)
+    k=ceil(log(CPU.TAILLE_STACK,2))
+    bits0=0
+    for i in range(univers.n3):
+        bits0+=2**i
+    bits1=0
+    for i in range(k):
+        bits1+=2**i
+    bits2=0
+    for i in range(univers.n2):
+        bits2+=2**i
+    stack_ptr=entier & bits1
+    stack=[]
+    for i in range(CPU.TAILLE_STACK):
+        stack.insert(0,(entier>>(k+i*univers.n2))&bits2)
+    ptr=(entier>>(k+(CPU.TAILLE_STACK)*univers.n2))&bits0
+    dx=(entier>>(k+CPU.TAILLE_STACK*univers.n2+1*univers.n3))&bits0
+    cx=(entier>>(k+CPU.TAILLE_STACK*univers.n2+2*univers.n3))&bits0
+    bx=(entier>>(k+CPU.TAILLE_STACK*univers.n2+3*univers.n3))&bits0
+    ax=(entier>>(k+CPU.TAILLE_STACK*univers.n2+4*univers.n3))&bits0
+    return CPU(ptr, univers, ax, bx, cx, dx,stack , stack_ptr)
 def photo(univers,file):
-	"""Ecrit son etat dans le fichier done en argument. Remplace le fichier s'il existait deja"""
-	#Dans l'ordre : nb CPUs, nuero CPU suivant, CPUs,nbCaseMemoire,Memoire
-	donnees=len(univers.liste_cpus).to_bytes(univers.b1,byteorder='big')
-	donnees+=univers.indice_cpu_actuel.to_bytes(univers.b1,byteorder='big')
-	
-	nextToSave=0
-	while 8 < len(univers.liste_cpus) - nextToSave:
-		temp=0
-		for i in range(8):
-			temp=CPUtoInt((temp<<univers.n1)+univers.liste_cpus[nextToSave])
-			nextToSave += 1
-		donnees+=temp.to_bytes(univers.n1,byteorder='big')
-	temp=0
-	for i in range(nextToSave,len(univers.liste_cpus)):
-		t=CPUtoInt(univers.liste_cpus[i])
-		temp=(temp<<univers.n1)+CPUtoInt(univers.liste_cpus[i])
-	k=(len(univers.liste_cpus)-nextToSave)*univers.n1
-	n=ceil(k/8.)
-	# print("n :",n,"/",k,log(temp,2))
-	donnees+=(temp<<(8*n-k)).to_bytes(n,byteorder='big')
+    """Ecrit son etat dans le fichier done en argument. Remplace le fichier s'il existait deja"""
+    #Dans l'ordre : nb CPUs, nuero CPU suivant, CPUs,nbCaseMemoire,Memoire
+    donnees=len(univers.liste_cpus).to_bytes(univers.b1,byteorder='big')
+    donnees+=univers.indice_cpu_actuel.to_bytes(univers.b1,byteorder='big')
 
-	donnees+=len(univers.memoire).to_bytes(univers.b2,byteorder='big')
-		
-	nextToSave=0
-	while 8 < len(univers.memoire) - nextToSave:
-		temp=0
-		for i in range(8):
-			temp=(temp<<univers.n2)+univers.memoire[nextToSave]
-			nextToSave += 1
-		donnees+=temp.to_bytes(univers.n2,byteorder='big')
-	
-	temp=0
-	for i in range(nextToSave,len(univers.memoire)):
-		temp=(temp<<univers.n2)+univers.memoire[i]
-	k=(len(univers.memoire)-nextToSave)*univers.n2
-	n=ceil(k/8)
-	donnees+=(temp<<(8*n-k)).to_bytes(n,byteorder='big')
-	
+    nextToSave=0
+    while 8 < len(univers.liste_cpus) - nextToSave:
+        temp=0
+        for i in range(8):
+            temp=CPUtoInt((temp<<univers.n1)+univers.liste_cpus[nextToSave])
+            nextToSave += 1
+        donnees+=temp.to_bytes(univers.n1,byteorder='big')
+    temp=0
+    for i in range(nextToSave,len(univers.liste_cpus)):
+        t=CPUtoInt(univers.liste_cpus[i])
+        temp=(temp<<univers.n1)+CPUtoInt(univers.liste_cpus[i])
+    k=(len(univers.liste_cpus)-nextToSave)*univers.n1
+    n=ceil(k/8.)
+    # print("n :",n,"/",k,log(temp,2))
+    donnees+=(temp<<(8*n-k)).to_bytes(n,byteorder='big')
 
-	f=open(file,'wb')
-	f.write(donnees)
-	# print(len(donnees),donnees)
-	f.close()
-	
+    donnees+=len(univers.memoire).to_bytes(univers.b2,byteorder='big')
+
+    nextToSave=0
+    while 8 < len(univers.memoire) - nextToSave:
+        temp=0
+        for i in range(8):
+            temp=(temp<<univers.n2)+univers.memoire[nextToSave]
+            nextToSave += 1
+        donnees+=temp.to_bytes(univers.n2,byteorder='big')
+
+    temp=0
+    for i in range(nextToSave,len(univers.memoire)):
+        temp=(temp<<univers.n2)+univers.memoire[i]
+    k=(len(univers.memoire)-nextToSave)*univers.n2
+    n=ceil(k/8)
+    donnees+=(temp<<(8*n-k)).to_bytes(n,byteorder='big')
+
+
+    f=open(file,'wb')
+    f.write(donnees)
+    # print(len(donnees),donnees)
+    f.close()
+
 def loadPhoto(univers,file):
-	"""Lit un etat dans le fichier donne en argument."""
-	#Dans l'ordre : nb CPUs, nuero CPU suivant, CPUs,nbCaseMemoire,Memoire
-	univers.liste_cpus=[]
-	f=open(file,'rb')
-	
-	lenCPU=int.from_bytes(f.read(univers.b1),byteorder='big')
-	univers.indice_cpu_actuel=int.from_bytes(f.read(univers.b1),byteorder='big')
-	
-	CPUs=f.read(ceil(univers.n1*lenCPU/8.))
-	k1=0
-	for k in range(lenCPU):
-		temp=0
-		while(k1<univers.n1*(k+1)):
-			l=k1//8
-			debut=k1-l*8
-			nombreALire=min(univers.n1*(k+1)-k1,8-debut)
-			temp= (temp<<nombreALire )+ (( (CPUs[l] << debut & 0b11111111)>>(8-nombreALire) ) & 0b11111111 )  #on veut lire de debut a debut+nombre a lire.
-			k1+=nombreALire
-		univers.liste_cpus.append(intToCPU(temp,univers))
-	
-	univers.memoire=[]
-	
-	lenMemoire=int.from_bytes(f.read(univers.b2),byteorder='big')
-	memory=f.read(ceil(univers.n2*lenMemoire/8.))
-	# print(memory)
-	k1=0
-	for k in range(lenMemoire):
-		temp=0
-		while(k1<univers.n2*(k+1)):
-			l=k1//8
-			debut=k1-l*8
-			nombreALire=min(univers.n2*(k+1)-k1,8-debut)
-			temp= (temp<<nombreALire )+ ( (memory[l] << debut & 0b11111111)>>(8-nombreALire) ) & 0b11111111   #on veut lire de debut a debut+nombre a lire.
-			k1+=nombreALire
-		univers.memoire.append(temp)
-	f.close()
+    """Lit un etat dans le fichier donne en argument."""
+    #Dans l'ordre : nb CPUs, nuero CPU suivant, CPUs,nbCaseMemoire,Memoire
+    univers.liste_cpus=[]
+    f=open(file,'rb')
+
+    lenCPU=int.from_bytes(f.read(univers.b1),byteorder='big')
+    univers.indice_cpu_actuel=int.from_bytes(f.read(univers.b1),byteorder='big')
+
+    CPUs=f.read(ceil(univers.n1*lenCPU/8.))
+    k1=0
+    for k in range(lenCPU):
+        temp=0
+        while(k1<univers.n1*(k+1)):
+            l=k1//8
+            debut=k1-l*8
+            nombreALire=min(univers.n1*(k+1)-k1,8-debut)
+            temp= (temp<<nombreALire )+ (( (CPUs[l] << debut & 0b11111111)>>(8-nombreALire) ) & 0b11111111 )  #on veut lire de debut a debut+nombre a lire.
+            k1+=nombreALire
+        univers.liste_cpus.append(intToCPU(temp,univers))
+
+    univers.memoire=[]
+
+    lenMemoire=int.from_bytes(f.read(univers.b2),byteorder='big')
+    memory=f.read(ceil(univers.n2*lenMemoire/8.))
+    # print(memory)
+    k1=0
+    for k in range(lenMemoire):
+        temp=0
+        while(k1<univers.n2*(k+1)):
+            l=k1//8
+            debut=k1-l*8
+            nombreALire=min(univers.n2*(k+1)-k1,8-debut)
+            temp= (temp<<nombreALire )+ ( (memory[l] << debut & 0b11111111)>>(8-nombreALire) ) & 0b11111111   #on veut lire de debut a debut+nombre a lire.
+            k1+=nombreALire
+        univers.memoire.append(temp)
+    f.close()
 
--- Instructions_Tierra.py
+++ Instructions_Tierra.py
@@ -10,188 +10,188 @@ LIMITE_RECHERCHE = 10
 longueur_pattern = 0
 
 def nop0(cpu):
-	pass
+    pass
 
 def nop1(cpu):
-	pass
+    pass
 
 def not0(cpu):
-	if cpu.cx % 2 == 0:
-		cpu.cx += 1
-	else:
-		cpu.cx -= 1
+    if cpu.cx % 2 == 0:
+        cpu.cx += 1
+    else:
+        cpu.cx -= 1
 
 def shl(c):
-	c.cx = (c.cx << 1)
+    c.cx = (c.cx << 1)
 
 def zero(c):
-	c.cx = 0
+    c.cx = 0
 
 def ifz(c):
-	#Si cx vaut 0, on ne fait rien car le CPU va ensuite de lui 
-	#meme incrementer son ptr
-	if c.cx != 0:
-		c.incrementer_ptr()
+    #Si cx vaut 0, on ne fait rien car le CPU va ensuite de lui
+    #meme incrementer son ptr
+    if c.cx != 0:
+        c.incrementer_ptr()
 
 def subCAB(c):
-	c.cx = c.ax - c.bx
+    c.cx = c.ax - c.bx
 
 def subAAC(c):
-	c.ax = c.ax - c.cx
+    c.ax = c.ax - c.cx
 
 def incA(c):
-	c.ax += 1
+    c.ax += 1
 
 def incB(c):
-	c.bx += 1
+    c.bx += 1
 
 def incC(c):
-	c.cx += 1
+    c.cx += 1
 
 def incD(c):
-	c.dx += 1
+    c.dx += 1
 
 def decA(c):
-	c.ax -= 1
+    c.ax -= 1
 
 def decB(c):
-	c.bx -= 1
+    c.bx -= 1
 
 def decC(c):
-	c.cx -= 1
+    c.cx -= 1
 
 def decD(c):
-	c.dx -= 1
+    c.dx -= 1
 
 def pushA(c):
-	c.push_stack(c.ax)
-	c.incrementer_stack_ptr()
+    c.push_stack(c.ax)
+    c.incrementer_stack_ptr()
 
 def pushB(c):
-	c.push_stack(c.bx)
-	c.incrementer_stack_ptr()
+    c.push_stack(c.bx)
+    c.incrementer_stack_ptr()
 
 def pushC(c):
-	c.push_stack(c.cx)
-	c.incrementer_stack_ptr()
+    c.push_stack(c.cx)
+    c.incrementer_stack_ptr()
 
 def pushD(c):
-	c.push_stack(c.dx)
-	c.incrementer_stack_ptr()
+    c.push_stack(c.dx)
+    c.incrementer_stack_ptr()
 
 def popA(c):
-	c.ax = c.pop_stack()
-	c.decrementer_stack_ptr()
+    c.ax = c.pop_stack()
+    c.decrementer_stack_ptr()
 
 def popB(c):
-	c.bx = c.pop_stack()
-	c.decrementer_stack_ptr()
+    c.bx = c.pop_stack()
+    c.decrementer_stack_ptr()
 
 def popC(c):
-	c.cx = c.pop_stack()
-	c.decrementer_stack_ptr()
+    c.cx = c.pop_stack()
+    c.decrementer_stack_ptr()
 
 def popD(c):
-	c.dx = c.pop_stack()
-	c.decrementer_stack_ptr()
+    c.dx = c.pop_stack()
+    c.decrementer_stack_ptr()
 
 def jmp(c):
-	try:
-		l_pattern, indice, i = trouver_template_complementaire(c, LIMITE_RECHERCHE)
-	except PatternNotFoundException as e:
-		c.ptr += e.l_pattern
-		print("pattern non trouve")
-	except NoPatternException:
-		print("pas de pattern apres l'instruction jmp")
-		return
-	else:
-		c.ptr = indice + l_pattern- 1 #on soustrait 1 car le ptr va ensuite etre incremente
+    try:
+        l_pattern, indice, i = trouver_template_complementaire(c, LIMITE_RECHERCHE)
+    except PatternNotFoundException as e:
+        c.ptr += e.l_pattern
+        print("pattern non trouve")
+    except NoPatternException:
+        print("pas de pattern apres l'instruction jmp")
+        return
+    else:
+        c.ptr = indice + l_pattern- 1 #on soustrait 1 car le ptr va ensuite etre incremente
 
 
 def jmpb(c):
-	try:
-		l_pattern, indice, i = trouver_template_complementaire_arriere(c, LIMITE_RECHERCHE)
-	except PatternNotFoundException as e:
-		c.ptr += e.l_pattern
-	except NoPatternException:
-		return
-	else:
-		print('truc')
-		c.ptr = indice + l_pattern - 1 #on soustrait 1 car le ptr va ensuite etre incremente
+    try:
+        l_pattern, indice, i = trouver_template_complementaire_arriere(c, LIMITE_RECHERCHE)
+    except PatternNotFoundException as e:
+        c.ptr += e.l_pattern
+    except NoPatternException:
+        return
+    else:
+        print('truc')
+        c.ptr = indice + l_pattern - 1 #on soustrait 1 car le ptr va ensuite etre incremente
 
 def call(c):
-	try:
-		l_pattern, indice, i = trouver_template_complementaire(c, LIMITE_RECHERCHE)
-	except NoPatternException:
-		c.push_stack(c.ptr+1)
-		c.incrementer_stack_ptr()#cf doc tierra sur le comportement de la fonction (j'ai un doute)
-	except PatternNotFoundException as e:
-		c.ptr += e.l_pattern
-	else:
-		c.push_stack(c.ptr + l_pattern + 1)
-		#c.incrementer_stack_ptr() #on stocke l'ANCIENNE adresse + l_pattern
-		c.ptr = indice + l_pattern - 1 #car on va a l'adresse apres le pattern
+    try:
+        l_pattern, indice, i = trouver_template_complementaire(c, LIMITE_RECHERCHE)
+    except NoPatternException:
+        c.push_stack(c.ptr+1)
+        c.incrementer_stack_ptr()#cf doc tierra sur le comportement de la fonction (j'ai un doute)
+    except PatternNotFoundException as e:
+        c.ptr += e.l_pattern
+    else:
+        c.push_stack(c.ptr + l_pattern + 1)
+        #c.incrementer_stack_ptr() #on stocke l'ANCIENNE adresse + l_pattern
+        c.ptr = indice + l_pattern - 1 #car on va a l'adresse apres le pattern
 
 
 def ret(c):
-	x = c.pop_stack()
-	#c.decrementer_stack_ptr()
-	c.ptr = x - 1 #car on va incrementer ensuite c.ptr
+    x = c.pop_stack()
+    #c.decrementer_stack_ptr()
+    c.ptr = x - 1 #car on va incrementer ensuite c.ptr
 
 def movDC(c):
-	c.dx = c.cx
+    c.dx = c.cx
 
 def movBA(c):
-	c.bx = c.ax
+    c.bx = c.ax
 
 def movii(c):
-	#sert a copier le contenu d'une case dans une autree
-	u = c.univers
-	c.bx = u.ind(c.bx)
-	c.ax = u.ind(c.ax)
-	u.memoire[c.ax] = u.memoire[c.bx]
+    #sert a copier le contenu d'une case dans une autree
+    u = c.univers
+    c.bx = u.ind(c.bx)
+    c.ax = u.ind(c.ax)
+    u.memoire[c.ax] = u.memoire[c.bx]
 
 def adr(c, fonc=trouver_template_complementaire):
-	try:
-		l_pattern, indice, i = trouver_template_complementaire(c, LIMITE_RECHERCHE)
-	except NoPatternException:
-		pass
-	except PatternNotFoundException as e:
-		pass
-	else:
-		c.ax = indice + l_pattern #car on stocke l'adresse suivant le pattern
+    try:
+        l_pattern, indice, i = trouver_template_complementaire(c, LIMITE_RECHERCHE)
+    except NoPatternException:
+        pass
+    except PatternNotFoundException as e:
+        pass
+    else:
+        c.ax = indice + l_pattern #car on stocke l'adresse suivant le pattern
 
 
 def adrb(c):
-	adr(c, trouver_template_complementaire_arriere)
+    adr(c, trouver_template_complementaire_arriere)
 
 def adrf(c):
-	adr(c, trouver_template_complementaire_avant)
-	
+    adr(c, trouver_template_complementaire_avant)
+
 #													NOUVELLES INSTRUCTIONS
 def new(c):
-	"Creer un nouveau cpu a l'endroit de ax"
-	c.univers.inserer_cpu(CPU.CPU(c.ax,c.univers))
+    "Creer un nouveau cpu a l'endroit de ax"
+    c.univers.inserer_cpu(CPU.CPU(c.ax,c.univers))
 
 def rand(c):
-	c.ax  = int(c.univers.TAILLE_MEMOIRE*random.random())
-	"Place dans c.ax une valeur aleatoire"
+    c.ax  = int(c.univers.TAILLE_MEMOIRE*random.random())
+    "Place dans c.ax une valeur aleatoire"
 
 def read(c):
-	"Lit l'instruction correspondant a l'adresse presente dans c.bx et la place dans la stack"
-	c.bx = c.univers.ind(c.bx)
-	c.push_stack(c.univers.memoire[c.bx])
-	c.incrementer_stack_ptr()
+    "Lit l'instruction correspondant a l'adresse presente dans c.bx et la place dans la stack"
+    c.bx = c.univers.ind(c.bx)
+    c.push_stack(c.univers.memoire[c.bx])
+    c.incrementer_stack_ptr()
 
 def write(c):
-	"Ecrit l'instruction au sommet de la pile dans l'adresse contenue dans c.ax"
-	c.ax = c.univers.ind(c.ax)
-	if random.random()>c.univers.mutation:
-		c.univers.memoire[c.ax] = c.pop_stack()
-	else:
-		c.univers.memoire[c.ax] = random.randint(39)
-	c.decrementer_stack_ptr()
-	
+    "Ecrit l'instruction au sommet de la pile dans l'adresse contenue dans c.ax"
+    c.ax = c.univers.ind(c.ax)
+    if random.random()>c.univers.mutation:
+        c.univers.memoire[c.ax] = c.pop_stack()
+    else:
+        c.univers.memoire[c.ax] = random.randint(39)
+    c.decrementer_stack_ptr()
+
 def HCF(c):
-	"Tue le cpu qui la lit"
-	c.die()
+    "Tue le cpu qui la lit"
+    c.die()
