6a91422abca9650d16717b67079bbcf4fb4f9c31
Lucas CLARTE <clarte.lucas@gmail.com>
2018-04-21 19:32:13 +0200

    Changement de Main.py

--- Main.py
+++ Main.py
@@ -11,29 +11,9 @@ import Experiment
 
 class Main:
 
-	def test(self):
-		print(len(self.U.liste_cpus))
-
-	def __initancien__(self):
-		nextSite = NextSite.NextSite()
-		self.U = Univers.Univers(nextSite)
-		self.stats = Statistiques.Statistiques(self.U)
-		self.U.insDict.initialize(Instructions.instructions)
-		eve = charger_genome('eve')
-		ancestor = self.U.insDict.toInts(eve)
-		self.U.addIndividual(0, ancestor)
-		c = CPU.CPU(0, self.U)
-		self.U.inserer_cpu(c)
-		self.replay = Enregistrement.Replay()
-		self.replay.univers = self.U
-		nom = "Enregs/enreg"
-		print(ancestor)
-		for i in range(100):
-			self.replay.openWrite(nom+str(i))
-			self.replay.runAndSave(5000)
-			self.test()
-
 	def __init__(selfself):
 		exp = Experiment.Experiment()
-		exp.experiment1(TAILLE_MEMOIRE=[10000, 20000])
+		exp.setLargeurCalculDensite(0)
+		exp.setFolderName("experiences_20_avril")
+		exp.experiment2SimpleNextSite([200], 1, 500)
 Main()
