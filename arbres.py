0145ec9a399d11e1c0b1466ab0e276b4297795e1
princessedu91 <34183745+princessedu91@users.noreply.github.com>
2018-04-30 10:31:09 +0200

    Update arbres.py

--- arbres.py
+++ arbres.py
@@ -13,8 +13,8 @@ import numpy as np
 
 
 def creer_arbre(n):
-    nextSite = NextSite(memLen=50000)
-    univ = Univers.Univers(nextSite, TAILLE_MEMOIRE=50000, mutation=0)
+    nextSite = NextSite.NextSite(memLen=5000)
+    univ = Univers.Univers(nextSite, TAILLE_MEMOIRE=5000, mutation=0)
     univ.LARGEUR_CALCUL_DENSITE = 23
     univ.insDict.initialize(Instructions.instructions) 
     eve = Enregistrement.charger_genome('eve') #charge le genome eve
@@ -23,7 +23,7 @@ def creer_arbre(n):
     c = CPU.CPU(0, univ)  #on ajoute un CPU pour lire le genome
     c.generation = 1
     univ.inserer_cpu(c)
-
+    nb=[]
     loc_cpus = dict()
     arbre=dict()
     univ.liste_cpus[0].id="0/1"
@@ -32,6 +32,7 @@ def creer_arbre(n):
 
     for j in range(n):
         univ.cycle()
+        nb+=[len(univ.liste_cpus)]
         k=0
         for k in univ.liste_cpus:
             if  k.id.split("/")[1] in loc_cpus:
@@ -43,7 +44,7 @@ def creer_arbre(n):
                 arbre[2*a]=0
                 arbre[2*a+1]=0
                
-    return(arbre)
+    return(arbre,nb)
  
 
 
@@ -51,12 +52,14 @@ def creer_arbre(n):
 def exp_arbre(n,m):
     t=[]
     for i in range(m):
-        t+=[dessiner(creer_arbre(n))]
+        arbre,nb=creer_arbre(n)
+        t+=[[arbre,nb]]
+        dessiner(arbre,nb)
     return(t)
  
 
    
-def dessiner(arbre):
+def dessiner(arbre,nb):
  
   
     l=list(arbre.keys())
@@ -79,13 +82,13 @@ def dessiner(arbre):
             else:
                 k=k+1
                 m[k]=[0,m[k//2][2],m[k//2][2]+arbre[k]]
-                maxi+=1
+                maxi+=max(1/nb[m[k][1]],(1.5)**(10-m[k][1]/500))
                 m[k][0]=maxi
 
                  
     for j in m:
-        plt.plot([m[j][0],m[j][0]],[m[j][1],m[j][2]])
+        plt.plot([m[j][0],m[j][0]],[m[j][1]/500,m[j][2]/500])
         if j!=1:
-            plt.plot([m[j//2][0],m[j][0]],[m[j][1],m[j][1]])
+            plt.plot([m[j//2][0],m[j][0]],[m[j][1]/500,m[j][1]/500])
     plt.show()     
 
